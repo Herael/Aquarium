4f383dc983d84c5328b6998885b7dc202638e8af
Herael <b.rousval@hotmail.com>
2020-01-26 18:51:37 +0100

    Initial commit

---
+++ Main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Benjamin Rousval on 26/01/2020.
+//
+
+#include "Main.h"
---
+++ Main.h
@@ -0,0 +1,16 @@
+//
+// Created by Benjamin Rousval on 26/01/2020.
+//
+
+#ifndef AQUARIUM_MAIN_H
+#define AQUARIUM_MAIN_H
+
+
+
+class Main {
+
+};
+
+
+
+#endif //AQUARIUM_MAIN_H
